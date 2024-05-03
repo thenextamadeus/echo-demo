9e42cc54417528eb688d82405287aca9d10d6fbf
Amadeus Cameron <evanamadeuscameron@gmail.com>
2024-05-03 19:51:01 -0400

    demo-file-initi

---
+++ demo/demo-echo-generate.py
@@ -0,0 +1,48 @@
+# Initialize libraries
+import os
+from playsound import playsound
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Activate env
+load_dotenv()
+apiKey = os.getenv("APIKEY")
+lang = 'en'
+client = OpenAI(api_key=apiKey)
+
+intent = "demo-echo.py is a demo day example of an extremely linear interaction with echo, WIZARD OF OZ PROTOTYPE"
+
+response_1 = "Heart Rate Dropped Significantly. please recording vitals at 1 minute intervals."
+FILENAME = "echo-says-suggestion.mp3"
+
+# Initialize a counter
+counter = 1
+
+def echoSpeaks(response):
+    global counter
+
+    # Increment the counter
+    filename = FILENAME
+    counter += 1
+
+    # Text to speech, using OpenAI
+    with client.audio.speech.with_streaming_response.create(
+        model="tts-1",
+        voice="nova",
+        input=str(response)
+    ) as response:
+        # This doesn't seem to be *actually* streaming, it just creates the file
+        # and then doesn't update it until the whole generation is finished
+        response.stream_to_file(filename)
+
+    playsound(filename)
+
+def main():
+    global counter
+
+    echoSpeaks(response_1)
+    print("response_1: ", response_1)
+
+# Run the main function
+if __name__ == "__main__":
+    main()  
---
+++ demo/demo-echo-responds.py
@@ -0,0 +1,80 @@
+import time
+from playsound import playsound
+from pynput import keyboard
+import speech_recognition as sr
+
+# Define global variables for audio file paths
+AUDIO_FILE_PATH_PRESS = "./demo-audio/echo-listening.wav"
+AUDIO_FILE_PATH_RELEASE = "./demo-audio/echo-heard.wav"
+AUDIO_FILE_PATH_NOTIFY = "./demo-audio/echo-notify.wav"
+AUDIO_FILE_PATH_ACKNOWLEDGE = "./demo-audio/echo-says-1.mp3"
+AUDIO_FILE_PATH_SUMMARY = "./demo-audio/echo-says-suggestion.mp3"
+
+# Global Parameters for Speech Recognition
+mic = sr.Microphone(0)
+r = sr.Recognizer()
+gPause = 0.7 # This represents the minimum length of silence (in seconds) that will register as the end of a phrase. The recognizer keeps listening until it encounters this duration of silence after speech.
+gPhrase = 3 # This is the maximum number of seconds that the recognizer will allow a phrase to continue before stopping and returning the audio captured until that point.
+
+# Trigger Detection Parameters (shorter = potential faster response)
+tPause = 0.5
+tPhrase = 3  
+
+# Boolean variable to track if spacebar is currently pressed
+spacebar_pressed = False
+
+
+# Function to recognize speech
+def recognizeSpeech():
+    r.pause_threshold = gPause
+    r.phrase_threshold = gPhrase
+
+    with mic as source:
+        print("Listening...")
+        audio = r.listen(source, phrase_time_limit=gPhrase)
+        print("Recognizing...")
+        try:
+            text = r.recognize_google(audio)
+            print(f"You said: {text}")  
+        except sr.UnknownValueError:
+            print("...")
+        except sr.RequestError as e:
+            print(f"error; {e}")
+
+
+# Function to handle key press event
+def on_press(key):
+    global spacebar_pressed
+    if key == keyboard.Key.space and not spacebar_pressed:
+        print('Spacebar pressed')
+        playsound(AUDIO_FILE_PATH_PRESS)
+        # recognizeSpeech()
+        spacebar_pressed = True
+
+# Function to handle key release event
+def on_release(key):
+    global spacebar_pressed
+    if key == keyboard.Key.space:
+        print('Spacebar released')
+        playsound(AUDIO_FILE_PATH_RELEASE)
+        playsound(AUDIO_FILE_PATH_ACKNOWLEDGE)
+        time.sleep(1)
+        playsound(AUDIO_FILE_PATH_NOTIFY)
+        playsound(AUDIO_FILE_PATH_SUMMARY)
+        spacebar_pressed = False
+    elif key == keyboard.Key.esc:
+        print('Exiting the program...')
+        return False  # Stop listener to exit the program
+
+# Start listener to detect key press and release events
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+# Main loop: Keep the program running
+while True:
+    # Do nothing, just keep the program running
+    time.sleep(1)
+
+    # When false is returned from on_release, exit the program
+    if not listener.running:
+        break
